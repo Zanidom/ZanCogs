ebfaf85801bc8381c954d872557e0c43b4508a8e
Zanidom <zanidom@gmail.com>
2023-08-05 23:38:07 +0100

    whoops wrong keyword

--- shop/__init__.py
+++ shop/__init__.py
@@ -3,6 +3,6 @@ from .shop import Shop
 __red_end_user_data_statement__ = "This cog stores discord IDs as needed for operation."
 
 
-await def setup(bot):
+async def setup(bot):
     cog = Shop()
     await bot.add_cog(cog)
