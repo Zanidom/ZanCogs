3e259e688b362acca91f59e8551c8736dfb46448
Zanidom <zanidom@gmail.com>
2023-08-06 00:20:16 +0100

    bigmoji 3.5.3

---
+++ bigmoji/__init__.py
@@ -0,0 +1,10 @@
+from .bigmoji import Bigmoji
+
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
+
+async def setup(bot):
+    cog = Bigmoji(bot)
+    await bot.add_cog(cog)
