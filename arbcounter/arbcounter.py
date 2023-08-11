c6d62d01f5dbf840a3feba229443330112336542
Zanidom <zanidom@gmail.com>
2023-08-12 00:35:26 +0100

    big updates for arbcounter

--- arbcounter/arbcounter.py
+++ arbcounter/arbcounter.py
@@ -1,3 +1,5 @@
+from http import server
+from turtle import right
 from redbot.core import Config, commands
 
 DEFAULT_COUNTER = {
@@ -7,46 +9,68 @@ DEFAULT_COUNTER = {
 class ArbCounter(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=1234567890, force_registration=True)
+        self.config = Config.get_conf(self, identifier=123456789011, force_registration=True)
         self.config.register_global(**DEFAULT_COUNTER)
+        default_guild = {}
+        self.config.register_guild(**default_guild)
 
     @commands.group(name="ac")
-    async def arbcounter(self, ctx):
+    async def arbcounter(self, ctx, *, lastMessage = ""):
         """Arbitrary counter commands"""
         if ctx.invoked_subcommand is None:
-            await ctx.send_help(ctx.command)
+            guild = ctx.guild
+            serverConfig = await self.config.guild(guild).Config()
+
+            mesSuffix = lastMessage[-2]
+
+            if mesSuffix == "++":
+                tokenName = lastMessage[:-2]
+                val = serverConfig[tokenName] + 1
+                serverConfig[tokenName] = val
+                await self.config.guild(guild).Config.set(serverConfig) #save our changes
+                await ctx.send(f"{lastMessage}` is now {val}.")
+            elif mesSuffix == "--":
+                tokenName = lastMessage[:-2]
+                val = serverConfig[tokenName] - 1
+                serverConfig[tokenName] = val
+                await self.config.guild(guild).Config.set(serverConfig) #save our changes
+                await ctx.send(f"{lastMessage}` is now {val}.")
+            else:
+                val = serverConfig[lastMessage]
+                if val is None:
+                    await ctx.send("This token has not been set.")
+                else:
+                    await ctx.send(f"The value of `{lastMessage}` is {val}.")
+                pass
+            pass
+        pass
 
     @arbcounter.command(name="set")
     async def ac_set(self, ctx, counterToken: str, value: int):
         """Set a value for a counter token"""
-        await self.config.counterToken.set(value)
+        guild = ctx.guild
+        serverConfig = await self.config.guild(guild).Config()
+        serverConfig[counterToken] = value
+        await self.config.guild(guild).Config.set(serverConfig) #save our changes
         await ctx.send(f"Set `{counterToken}` to {value}.")
 
     @arbcounter.command(name="delete")
     async def ac_delete(self, ctx, counterToken: str):
         """Delete a counter token"""
+        
+        guild = ctx.guild
+        serverConfig = await self.config.guild(guild).Config()
+        del serverConfig[counterToken]
         await self.config.clear_raw(counterToken)
+        await self.config.guild(guild).Config.set(serverConfig) #save our changes
         await ctx.send(f"Deleted `{counterToken}`.")
 
     @arbcounter.command(name="++")
     async def ac_increment(self, ctx, counterToken: str):
-        """Increment a counter token by 1"""
-        current_value = await self.config.counterToken()
-        await self.config.counterToken.set(current_value + 1)
-        await ctx.send(f"Incremented `{counterToken}` to {current_value + 1}.")
+        """Increment a counter token by 1 - ac tokenname++"""
+        pass
 
     @arbcounter.command(name="--")
     async def ac_decrement(self, ctx, counterToken: str):
-        """Decrement a counter token by 1"""
-        current_value = await self.config.counterToken()
-        await self.config.counterToken.set(current_value - 1)
-        await ctx.send(f"Decreased `{counterToken}` to {current_value - 1}.")
-
-    @arbcounter.command()
-    async def ac(self, ctx, counterToken: str):
-        """Show the current value of a counter token"""
-        value = await self.config.counterToken()
-        if value is None:
-            await ctx.send("This token has not been set.")
-        else:
-            await ctx.send(f"The value of `{counterToken}` is {value}.")
+        """Decrement a counter by 1 - ac tokenname--"""
+        pass
