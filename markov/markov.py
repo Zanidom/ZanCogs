40037ceeed084e5ea88aeaf8a275a1cdd155f64e
Zanidom <zanidom@gmail.com>
2023-09-25 21:18:41 +0100

    more change

--- markov/markov.py
+++ markov/markov.py
@@ -218,7 +218,7 @@ class Markov(commands.Cog):
             try:
                 gram = await generator(model, state)
             except:
-                gram = " "
+                return
             output.append(gram)
             # Produce sliding state window (ngram)
             i += 1
